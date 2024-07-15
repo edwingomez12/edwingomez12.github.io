651c2b4335b29cdab97f2ff2b8d0bf3cc9e56aee
edwingomez12 <106348997+edwingomez12@users.noreply.github.com>
2024-07-15 17:25:08 -0500

    Fixing styles

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,18 +1,31 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import './Header.css';
 
 const Header = () => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+
   return (
     <header className="header">
       <div className="container">
-
-        <nav className="nav">
+        <div className="brand">
+          <Link to="/">Edwin Gomez</Link>
+        </div>
+        <div className={`nav-toggle ${isMenuOpen ? 'open' : ''}`} onClick={toggleMenu}>
+          <div className="bar"></div>
+          <div className="bar"></div>
+          <div className="bar"></div>
+        </div>
+        <nav className={`nav ${isMenuOpen ? 'open' : ''}`}>
           <ul>
-            <li><Link to="/">Home</Link></li>
-            <li><Link to="/projects">Experience</Link></li>
-            <li><a href="https://drive.google.com/file/d/1kF_FK0N3_86Yb2D_ujZKn_c9qVK0bJtZ/view?usp=drive_link" target="_blank" rel="noopener noreferrer">Resume</a></li>
-            <li><Link to="/contact">Contact</Link></li>
+            <li><Link to="/" onClick={toggleMenu}>Home</Link></li>
+            <li><Link to="/projects" onClick={toggleMenu}>Experience</Link></li>
+            <li><a href="https://drive.google.com/file/d/1kF_FK0N3_86Yb2D_ujZKn_c9qVK0bJtZ/view?usp=drive_link" target="_blank" rel="noopener noreferrer" onClick={toggleMenu}>Resume</a></li>
+            <li><Link to="/contact" onClick={toggleMenu}>Contact</Link></li>
           </ul>
         </nav>
       </div>
