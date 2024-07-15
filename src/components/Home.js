f0b8a188b3f50cea6d6521a01376136a8112969a
edwingomez12 <106348997+edwingomez12@users.noreply.github.com>
2024-07-15 16:18:46 -0500

    update home css

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,41 +1,37 @@
-import React, {useEffect, useState }from 'react';
-import { ReactTyped } from "react-typed";
-
+import React, { useEffect, useState } from 'react';
+import {ReactTyped} from 'react-typed';
 import './Home.css';
 
-
 const Home = () => {
-
-    const [imageLoaded, setImageLoaded] = useState(false);
+  const [imageLoaded, setImageLoaded] = useState(false);
 
   useEffect(() => {
     const img = new Image();
     img.onload = () => {
       setImageLoaded(true);
     };
-    img.src = `${process.env.PUBLIC_URL}/Me.jpeg`; 
+    img.src = `${process.env.PUBLIC_URL}/Me.jpeg`; // Ensure correct path
   }, []);
 
   return (
     <section className="home">
       <div className="left-side">
-      <div
+        <div
           className={`image ${imageLoaded ? 'loaded' : ''}`}
           style={{ backgroundImage: `url(${process.env.PUBLIC_URL}/Me.jpeg)` }}
-        ></div>    
-          </div>
+        ></div>
+      </div>
       <div className="right-side">
         <div className="content">
-        <h1>Edwin Gomez
-        </h1>
-        <h2>
-          <ReactTyped
-            strings={['Software Engineer', '5 years of experience', 'Bachelors in Science Computer Engineering']}
-            typeSpeed={40}
-            backSpeed={50}
-            loop
-          />
-        </h2>
+          <h1>Edwin Gomez</h1>
+          <h2>
+            <ReactTyped
+              strings={['Software Engineer', '5 years of experience', 'Bachelors in Science Computer Engineering']}
+              typeSpeed={40}
+              backSpeed={50}
+              loop
+            />
+          </h2>
         </div>
       </div>
     </section>
