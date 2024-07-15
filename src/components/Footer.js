1b04beb8b69f97427dd0e73fd2db36a29c1d5ea9
edwingomez12 <106348997+edwingomez12@users.noreply.github.com>
2024-07-15 16:13:36 -0500

    changed footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,7 +5,7 @@ const Footer = () => {
   return (
     <footer className="footer">
       <div className="container">
-        <p>&copy; 2024 My Portfolio</p>
+        <p>&copy; Edwin Gomez</p>
       </div>
     </footer>
   );
