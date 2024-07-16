7efc52135439082a0c462c9d7672bdf318f5ff76
edwingomez12 <106348997+edwingomez12@users.noreply.github.com>
2024-07-16 18:49:18 -0500

    Update home

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,25 +3,11 @@ import {ReactTyped} from 'react-typed';
 import './Home.css';
 
 const Home = () => {
-  const [imageLoaded, setImageLoaded] = useState(false);
-
-  useEffect(() => {
-    const img = new Image();
-    img.onload = () => {
-      setImageLoaded(true);
-    };
-    img.src = `${process.env.PUBLIC_URL}/Me.jpeg`; // Ensure correct path
-  }, []);
+ 
 
   return (
     <section className="home">
-      <div className="left-side">
-        <div
-          className={`image ${imageLoaded ? 'loaded' : ''}`}
-          style={{ backgroundImage: `url(${process.env.PUBLIC_URL}/Me.jpeg)` }}
-        ></div>
-      </div>
-      <div className="right-side">
+      <div className="center-side">
         <div className="content">
           <h1>Edwin Gomez</h1>
           <h2>
